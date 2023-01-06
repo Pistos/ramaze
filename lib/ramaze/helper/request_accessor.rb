f3f4cfc2232c98a9d25f89538caa9515795cb3d7
Michael Faughn <michael.faughn@nist.gov>
2023-01-06 10:51:53 -0700

    accomodate changes to Rack::Request that refactor the Helpers and Env modules so that request_accessor.rb works

--- lib/ramaze/helper/request_accessor.rb
+++ lib/ramaze/helper/request_accessor.rb
@@ -1,12 +1,14 @@
+require 'rack/request'
 module Ramaze
   module Helper
     module RequestAccessor
-      classes = [Rack::Request, Innate::Request, Ramaze::Request]
+      classes = [Rack::Request, Rack::Request::Helpers, Rack::Request::Env, Innate::Request, Ramaze::Request]
       methods = classes.map { |klass|
         klass.instance_methods(false)
       }.flatten.uniq
 
       methods.each do |method|
+        next if method == :intialize
         if method =~ /=/
           eval("def %s(a) request.%s a; end" % [method, method])
         else
